0f886374a681dd1e4f10a7f73f45f6541a1aad55
Matt Kuhn <mlkkuhn@live.com>
2025-05-28 08:05:33 -0400

    dont use promises and async coding to update model

--- game.js
+++ game.js
@@ -1,22 +1,33 @@
-getPlayerTotalForcesValue = () => Number(getPlayerTotalForcesElement().value);
-getPlayerTotalForcesElement = () => document.querySelector("#player-total-forces");
+elt = (id) => {
+    return document.getElementById(id);
+}
 
-getOpponentTotalForcesValue = () => Number(getOpponentTotalForcesElement().value);
-getOpponentTotalForcesElement = () => document.querySelector("#opponent-total-forces");
+val = (id, value) => {
+    const x = elt(id);
+    if (value !== undefined) {
+        if (x.tagName.toLowerCase() === "input") {
+            x.value = value.toString();
+        } else {
+            x.textContent = value.toString();
+        }
+    } else {
+        if (x.tagName.toLowerCase() === "input") {
+            return Number(x.value);
+        } else {
+            return Number(x.textContent);
+        }
+    }
 
-getPlayerCommitForcesValue = () => Number(getPlayerCommitForcesElement().value);
-getPlayerCommitForcesElement = () => document.querySelector("#player-commit-forces");
+}
 
-getOpponentCommitForcesValue = () => Number(getOpponentCommitForcesElement().value);
-getOpponentCommitForcesElement = () => document.querySelector("#opponent-commit-forces");
-
-getPlayerVictoriesValue = () => Number(getPlayerVictoriesElement().value);
-getPlayerVictoriesElement = () => document.querySelector("#player-victories");
-
-getOpponentVictoriesValue = () => Number(getOpponentVictoriesElement().value);
-getOpponentVictoriesElement = () => document.querySelector("#opponent-victories");
-
-getOutcomeElement = () => document.querySelector("#outcome");
+playerTotalForcesId = () => "player-total-forces";
+opponentTotalForcesId = () => "opponent-total-forces";
+playerCommitForcesId = () => "player-commit-forces";
+opponentCommitForcesId = () => "opponent-commit-forces";
+playerVictoriesId = () => "player-victories";
+opponentVictoriesId = () => "opponent-victories";
+outcomeId = () => "outcome";
+opponentActionsId = () => "opponent-actions";
 
 playerForceCommitmentSubmit = async (event) => {
     event.preventDefault();
@@ -27,42 +38,30 @@ doGameStepAttack = () => {
     return {
         playerVictory: undefined,
         validate: async () => {
-            const valid = getPlayerCommitForcesValue() <= getPlayerTotalForcesValue();
-            getPlayerCommitForcesElement().setCustomValidity(valid ? "" : "You don't have those forces at your disposal, sir!");
-            getPlayerCommitForcesElement().reportValidity();
+            const valid = val(playerCommitForcesId()) <= val(playerTotalForcesId());
+            elt(playerCommitForcesId()).setCustomValidity(valid ? "" : "You don't have those forces at your disposal, sir!");
+            elt(playerCommitForcesId()).reportValidity();
             return valid;
         },
         gameStep: async () => {
             await doOpponentAction(opponentActionAssignForces());
-            this.playerVictory = getPlayerCommitForcesValue() > getOpponentCommitForcesValue();
+            this.playerVictory = val(playerCommitForcesId()) > val(opponentCommitForcesId());
         },
         post: async () => {
-            getOutcomeElement().textContent = this.playerVictory ? "Victory!" : "Defeat!";
+            val(outcomeId(), this.playerVictory ? "Victory!" : "Defeat!");
 
-            await setValue(getPlayerTotalForcesElement(), `${getPlayerTotalForcesValue() - getPlayerCommitForcesValue()}`);
-            await setValue(getOpponentTotalForcesElement(), `${getOpponentTotalForcesValue() - getOpponentCommitForcesValue()}`);
-            await setValue(getPlayerCommitForcesElement(), "0");
-            await setValue(getOpponentCommitForcesElement(), "0");
+            val(playerTotalForcesId(), val(playerTotalForcesId()) - val(playerCommitForcesId()));
+            val(opponentTotalForcesId(), val(opponentTotalForcesId()) - val(opponentCommitForcesId()));
+            val(playerCommitForcesId(), 0);
+            val(opponentCommitForcesId(), 0);
 
             if (this.playerVictory) {
-                await setValue(getPlayerVictoriesElement(), `${getPlayerVictoriesValue()+1}`)
+                val(playerVictoriesId(), val(playerVictoriesId())+1);
             } else {
-                await setValue(getOpponentVictoriesElement(), `${getOpponentVictoriesValue()+1}`)
+                val(opponentVictoriesId(), val(opponentVictoriesId())+1);
             }
         }
     }
-}
-
-setValue = async (element, to) => {
-    const color = element.style.color;
-    element.style.color = "red";
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            element.style.color = color;
-            element.value = to;
-            resolve();
-        }, 1000);
-    })
 }
 
 doGameStep = async (gameStep) => {
@@ -72,8 +71,10 @@ doGameStep = async (gameStep) => {
     
     document.querySelectorAll("input").forEach(x => x.setAttribute("readonly", true));
     document.querySelectorAll("button").forEach(x => x.setAttribute("disabled", true));
+
     await gameStep.gameStep();
     await gameStep.post();
+
     document.querySelectorAll("input").forEach(x => x.removeAttribute("readonly"));
     document.querySelectorAll("button").forEach(x => x.removeAttribute("disabled"));
 }
@@ -83,22 +84,35 @@ opponentActionAssignForces = () => {
         startText: "Thinking...",
         readyText: "Ready!",
         fn: async () => {
-            const forces = Math.floor(Math.random() * getOpponentTotalForcesValue());
-            await setValue(getOpponentCommitForcesElement(), `${forces}`);
+            const forces = Math.floor(Math.random() * val(opponentTotalForcesId()));
+            val(opponentCommitForcesId(), forces);
         }
     }
 }
 
 doOpponentAction = async (action) => {
     return new Promise((resolve) => {
-        document.querySelector("#opponent-actions").textContent = action.startText;
+        val(opponentActionsId(), action.startText);
         setTimeout(() => {
-            document.querySelector("#opponent-actions").textContent = action.readyText;
+            val(opponentActionsId(), action.readyText);
             setTimeout(async () => {
-                document.querySelector("#opponent-actions").textContent = "";
+                val(opponentActionsId(), "");
                 await action.fn();
                 resolve();
             }, 500)
         }, 1000);
     });
 }
+
+// TODO : can this be used for ui purposes, still?
+// setValue = async (element, to) => {
+//     const color = element.style.color;
+//     element.style.color = "red";
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             element.style.color = color;
+//             element.value = to;
+//             resolve();
+//         }, 1000);
+//     })
+// }
