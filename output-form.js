a28c128a002bdcf3ced564636aa6af840a8bc02e
Matt Kuhn <mlkkuhn@live.com>
2025-05-28 15:53:16 -0400

    update comment

--- output-form.js
+++ output-form.js
@@ -1,15 +1,22 @@
+/**
+ * Includes <output> elements in a form submission.
+ * the output's id and textContent are included in the form data as hidden inputs.
+ * Usage:
+ * ```html
+ * <form is="output-form">
+ *   <output id="result">42</output>
+*    <button type="submit">Submit</button>
+ * </form>
+ * 
+ */
 class OutputForm extends HTMLFormElement {
-    constructor() {
-        super();
-    }
-
     connectedCallback() {
         this.addEventListener('submit', (e) => {
             this.querySelectorAll('output[id]').forEach(output => {
                 const hidden = document.createElement('input');
                 hidden.type = 'hidden';
                 hidden.name = output.id;
-                hidden.value = output.value || output.textContent;
+                hidden.value = output.textContent;
                 this.appendChild(hidden);
             });
         });
